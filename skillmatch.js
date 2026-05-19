eb04fecc9f3e6178a0ae989525721b649c9e374e
donerandrade <donerandrade@gmail.com>
2026-05-19 18:26:19 -0300

    RF01 - Criando perfil do candidato

---
+++ skillmatch.js
@@ -0,0 +1,8 @@
+// RF01 - Criando perfil do candidato
+
+const candidato = {
+    nome: "Doner Andrade",
+    area: "Front-End",
+    habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "kanban"];
+    experineciaMeses: 3
+}
