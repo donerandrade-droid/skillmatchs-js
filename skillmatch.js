288fb99336fd4aaa1617988479a81b7929f4b94c
donerandrade <donerandrade@gmail.com>
2026-05-20 17:29:19 -0300

    RF02 - Criando uma lista de vagsa

--- skillmatch.js
+++ skillmatch.js
@@ -1,4 +1,5 @@
 // RF01 - Criando perfil do candidato
+// array e objeto
 
 const candidato = {
     nome: "Doner Andrade",
@@ -6,3 +7,34 @@ const candidato = {
     habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "kanban"];
     experineciaMeses: 3
 }
+
+// RF02 - Criando uma lista de vagas
+// array, objeto, array de objetos, 
+const vagas = [
+    {
+        id: 1,
+        empresa: "TechStart",
+        cargo: "Desenvolvedor Front-End Júnior",
+        requisitos: ["JavaScript", "GitHub", "Lógica de Programação"],
+        salario: 2800,
+        modalidade: "Remoto",
+    },
+    {
+        id: 2,
+        empresa: "CodeLab",
+        cargo: "Estágio Front-End",
+        requisitos: ["JavaScript", "Kanban", "GitHub"],
+        salario: 1800,
+        modalidade: "Hibrido",
+    },
+    {
+        id: 3,
+        empresa: "WebSolutions",
+        cargo: "Programador JavaScript Júnior",
+        requisitos: ["JavaScript", "Arrays", "Objetos", "Funcões"],
+        salario: 3000,
+        modalidade: "Presencial",
+    },
+
+
+]
