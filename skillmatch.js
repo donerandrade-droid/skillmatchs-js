973aaa411742bb3d3e84df7b642f082d5bc2f744
donerandrade <donerandrade@gmail.com>
2026-05-20 18:16:43 -0300

    RF03 - Compatibilidade  -RF04 Classificar vagas por compatibilidade

--- skillmatch.js
+++ skillmatch.js
@@ -4,9 +4,9 @@
 const candidato = {
     nome: "Doner Andrade",
     area: "Front-End",
-    habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "kanban"];
-    experineciaMeses: 3
-}
+    habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "kanban"],
+    experineciaMeses: 3,
+};
 
 // RF02 - Criando uma lista de vagas
 // array, objeto, array de objetos, 
@@ -34,7 +34,43 @@ const vagas = [
         requisitos: ["JavaScript", "Arrays", "Objetos", "Funcões"],
         salario: 3000,
         modalidade: "Presencial",
-    },
+    }
+
+];
+
+// RF03 - Calcular compatibilidade com cada vaga
+
+function analisarVaga(vaga) {
+    const habilidadesEncontradas = vaga.requisitos.filter(req =>
+        candidato.habilidades.includes(req)
+    );
+    const habilidadesFaltantes = vaga.requisitos.filter(req =>
+        !candidato.habilidades.includes(req)
+    );
+
+    const totalRequisitos = vaga.requisitos.length;
+    const compatibilidade = Math.round((habilidadesEncontradas.length / totalRequisitos) * 100);
 
 
-]
+// RF04 - Classificar vagas por compatibilidade
+        
+let classificacao = "";
+if (compatibilidade >= 80) {
+    classificacao = "Alta Compatibilidade";
+} else if (compatibilidade >= 50) {
+    classificacao = "Média Compatibilidade";
+} else {
+    classificacao = "Baixa Compatibilidade";
+}
+
+// RF05 - Exibir resultados
+  console.log('Nome do candidato: ' + candidato.nome);
+    console.log(`Empresa: ${vaga.empresa}`);
+    console.log(`Cargo: ${vaga.cargo}`);
+    console.log(`Compatibilidade: ${compatibilidade}%`);
+    console.log(`Habilidades encontradas: ${habilidadesEncontradas.join(", ")}`);
+    console.log(`Habilidades faltantes: ${habilidadesFaltantes.join(", ") || "Nenhuma!"}`);
+    console.log(`Classificação: ${classificacao}`);
+    console.log("---");
+}
+
